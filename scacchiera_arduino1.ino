703348249e6a2115efb6cf307597345cd9c0c584
fablabpartanna19 <51207657+fablabpartanna19@users.noreply.github.com>
2019-05-30 18:51:31 +0200

    Add files via upload

--- scacchiera_arduino1.ino
+++ scacchiera_arduino1.ino
@@ -33,6 +33,7 @@ const int ldr[8] = {A0, A1, A2, A3,A4,A5,A6,A7};
     digitalWrite( vccPin[i],LOW);  
        Serial.println(); 
    }
+    Serial.println("Nuova lettura"); 
           
  } // fim do loop principal  
  void estabeleceContato() {  
