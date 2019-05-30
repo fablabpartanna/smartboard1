ef1eef78cf5bf303d1d0a5aec8e0595baf615b1d
fablabpartanna19 <51207657+fablabpartanna19@users.noreply.github.com>
2019-05-30 18:42:44 +0200

    Add files via upload

---
+++ scacchiera_arduino1.ino
@@ -0,0 +1,43 @@
+const int ldr[8] = {A0, A1, A2, A3,A4,A5,A6,A7};  
+ const int vccPin[8] = {2,3,4,5,6,8,9,10};  
+ int ldrValue;  
+ void setup() {  
+  Serial.begin(9600);  
+  pinMode(vccPin[0], OUTPUT);   
+  pinMode(vccPin[1], OUTPUT);   
+  pinMode(vccPin[2], OUTPUT);   
+    pinMode(vccPin[3], OUTPUT);   
+  pinMode(vccPin[4], OUTPUT);   
+  pinMode(vccPin[5], OUTPUT);  
+    pinMode(vccPin[6], OUTPUT);   
+  pinMode(vccPin[7], OUTPUT);   
+  
+ }  
+ void loop() {  
+   for(int i = 0; i < 8; i++)  
+   { 
+
+    digitalWrite( vccPin[i],HIGH);  
+      for(int j = 0; j < 8; j++)  
+      {  
+       ldrValue = analogRead(ldr[j]); 
+       ldrValue = constrain(ldrValue, 0, 800); // ajusta valor lido  
+       ldrValue = map(ldrValue,0,800,0,255); // le o LDR correspondente  
+      // ldrValue = constrain(ldrValue, 0, 800); // ajusta valor lido  
+     //  ldrValue = map(ldrValue,0,800,0,255);  // ajusta valor lido  
+       Serial.print(ldrValue);
+       Serial.print("  ");
+       delay(500);// envia valor via porta serial   
+       
+      }  
+    digitalWrite( vccPin[i],LOW);  
+       Serial.println(); 
+   }
+          
+ } // fim do loop principal  
+ void estabeleceContato() {  
+   while (Serial.available() <= 0) {  
+   Serial.write('A');  // envia A ate estabelecer contato  
+   delay(300);  
+  }  
+  }  
